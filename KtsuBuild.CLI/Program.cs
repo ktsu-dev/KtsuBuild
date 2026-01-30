6115489b1d8db4c9b9cae6e5fa2bb27d2bc1286f
Matt Edmondson <matthew.edmondson@gmail.com>
2026-01-30 16:22:11 +1100

    Initial files

---
+++ KtsuBuild.CLI/Program.cs
@@ -0,0 +1,17 @@
+// Copyright (c) ktsu.dev
+// All rights reserved.
+// Licensed under the MIT license.
+
+namespace KtsuBuild.CLI;
+
+using System.CommandLine;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+internal class Program
+{
+	public static async Task<int> Main(string[] args)
+	{
+	}
+}
